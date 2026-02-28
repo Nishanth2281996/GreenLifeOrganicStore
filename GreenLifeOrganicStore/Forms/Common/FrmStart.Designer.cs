4f667eae1f177f2126dd3d52ee3db9fa4018b4b1
Nishanth Masilamani <nishanthmasilamani7@gmail.com.com>
2026-02-28 06:40:48 +0530

    Add project files.

---
+++ GreenLifeOrganicStore/Forms/Common/FrmStart.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace GreenLifeOrganicStore
+{
+    partial class FrmStart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmStart));
+            this.panelCard = new System.Windows.Forms.Panel();
+            this.lblFooter = new System.Windows.Forms.Label();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblTagLine = new System.Windows.Forms.Label();
+            this.picLogo = new System.Windows.Forms.PictureBox();
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.lblHeaderTitle = new System.Windows.Forms.Label();
+            this.panelCard.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
+            this.panelHeader.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelCard
+            // 
+            this.panelCard.AutoScroll = true;
+            this.panelCard.AutoSize = true;
+            this.panelCard.BackColor = System.Drawing.Color.White;
+            this.panelCard.Controls.Add(this.lblFooter);
+            this.panelCard.Controls.Add(this.btnRegister);
+            this.panelCard.Controls.Add(this.btnLogin);
+            this.panelCard.Controls.Add(this.lblTagLine);
+            this.panelCard.Controls.Add(this.picLogo);
+            this.panelCard.Controls.Add(this.panelHeader);
+            this.panelCard.Location = new System.Drawing.Point(252, 30);
+            this.panelCard.Name = "panelCard";
+            this.panelCard.Size = new System.Drawing.Size(638, 575);
+            this.panelCard.TabIndex = 1;
+            // 
+            // lblFooter
+            // 
+            this.lblFooter.AutoSize = true;
+            this.lblFooter.Font = new System.Drawing.Font("Segoe UI", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFooter.ForeColor = System.Drawing.Color.Gray;
+            this.lblFooter.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lblFooter.Location = new System.Drawing.Point(212, 534);
+            this.lblFooter.Name = "lblFooter";
+            this.lblFooter.Size = new System.Drawing.Size(297, 19);
+            this.lblFooter.TabIndex = 5;
+            this.lblFooter.Text = "Version 1.0.0 | © 2026 GreenLife Organic Store";
+            this.lblFooter.Click += new System.EventHandler(this.lblFooter_Click);
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnRegister.ForeColor = System.Drawing.Color.White;
+            this.btnRegister.Location = new System.Drawing.Point(35, 448);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(569, 56);
+            this.btnRegister.TabIndex = 4;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = false;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(35, 386);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(569, 56);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // lblTagLine
+            // 
+            this.lblTagLine.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblTagLine.ForeColor = System.Drawing.Color.DimGray;
+            this.lblTagLine.Location = new System.Drawing.Point(149, 347);
+            this.lblTagLine.Name = "lblTagLine";
+            this.lblTagLine.Size = new System.Drawing.Size(338, 25);
+            this.lblTagLine.TabIndex = 2;
+            this.lblTagLine.Text = "Your trusted partner for organic products";
+            this.lblTagLine.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // picLogo
+            // 
+            this.picLogo.Image = ((System.Drawing.Image)(resources.GetObject("picLogo.Image")));
+            this.picLogo.Location = new System.Drawing.Point(142, 83);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(345, 252);
+            this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picLogo.TabIndex = 1;
+            this.picLogo.TabStop = false;
+            // 
+            // panelHeader
+            // 
+            this.panelHeader.BackColor = System.Drawing.Color.ForestGreen;
+            this.panelHeader.Controls.Add(this.lblHeaderTitle);
+            this.panelHeader.Location = new System.Drawing.Point(0, 0);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Size = new System.Drawing.Size(635, 77);
+            this.panelHeader.TabIndex = 0;
+            // 
+            // lblHeaderTitle
+            // 
+            this.lblHeaderTitle.AutoSize = true;
+            this.lblHeaderTitle.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeaderTitle.ForeColor = System.Drawing.Color.White;
+            this.lblHeaderTitle.Location = new System.Drawing.Point(122, 18);
+            this.lblHeaderTitle.Name = "lblHeaderTitle";
+            this.lblHeaderTitle.Size = new System.Drawing.Size(420, 48);
+            this.lblHeaderTitle.TabIndex = 0;
+            this.lblHeaderTitle.Text = "GreenLife Organic Store";
+            // 
+            // FrmStart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(1142, 704);
+            this.Controls.Add(this.panelCard);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FrmStart";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GreenLife Organic Store";
+            this.panelCard.ResumeLayout(false);
+            this.panelCard.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
+            this.panelHeader.ResumeLayout(false);
+            this.panelHeader.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelCard;
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.Label lblHeaderTitle;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblTagLine;
+        private System.Windows.Forms.PictureBox picLogo;
+        private System.Windows.Forms.Label lblFooter;
+    }
+}
+
---
+++ GreenLifeOrganicStore/Forms/Common/FrmStart.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GreenLifeOrganicStore
+{
+    public partial class FrmStart : Form
+    {
+        public FrmStart()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblFooter_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            var frm = new FrmLogin();
+            frm.Show();
+            this.Hide();
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            var frm = new FrmRegister();
+            frm.Show();
+            this.Hide();
+        }
+    }
+}
