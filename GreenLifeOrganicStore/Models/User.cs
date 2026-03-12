45c72a32beeed73a662cf39877776dbf7feaf90f
Nishanth Masilamani <nishanthmasilamani7@gmail.com.com>
2026-03-12 13:14:52 +0530

    Hashing Password Completed

--- GreenLifeOrganicStore/Models/User.cs
+++ GreenLifeOrganicStore/Models/User.cs
@@ -9,10 +9,11 @@ namespace GreenLifeOrganicStore.Models
     public class User
     {
         public int Users_id { get; set; }
-        public string Full_name { get; set; }
+        public string Full_Name { get; set; }
         public string U_Email {  get; set; }
         public string U_Phone { get; set;}
         public string U_City { get; set; }
+        public string U_Address {  get; set; }
         public byte[] Password_Hash {  get; set; }
         public byte[] Password_Salt { get; set; }
         public string ProfileImagePath {  get; set; }
--- GreenLifeOrganicStore/Services/PasswordHelper.cs
+++ GreenLifeOrganicStore/Services/PasswordHelper.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace GreenLifeOrganicStore.Services
 {
-    internal class PasswordHelper
+    public class PasswordHelper
     {
+        private const int SaltSize = 32;
+        private const int HashSize = 64;
+        private const int Iterations = 10000;
+
+        public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                passwordSalt = new byte[SaltSize];
+                rng.GetBytes(passwordSalt);
+            }
+
+            using (var pbkdf2 = new Rfc2898DeriveBytes(password, passwordSalt, Iterations))
+            {
+                passwordHash = pbkdf2.GetBytes(HashSize);
+            }
+        }
+
+          public bool VerifyPassword(string password , byte[] storedHash , byte[] storedSalt)
+        {
+            using (var pbkdf2 = new Rfc2898DeriveBytes(password, storedSalt, Iterations))
+            {
+                byte[] computedHash = pbkdf2.GetBytes(HashSize);
+
+                for (int i = 0; i < computedHash.Length; i++) {
+                    if (computedHash[i] != storedHash[i]) {
+                        return false;
+                    }
+            }
+        }
+
+            return true;
+        }
     }
 }
